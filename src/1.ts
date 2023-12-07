515f3949b665b49bfb1a874936afbb5d651df5c5
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2023-12-07 22:53:23 +0100

    add hw

--- src/1.ts
+++ src/1.ts
@@ -5,15 +5,11 @@
 */
 
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
+  constructor(public name: string, public age: number, public grade: string) {}
+  show(): void {
+    console.log(this.name, this.age, this.grade);
   }
 }
+new Student("Tony", 16, "2");
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  constructor(protected key: Key) {}
+  door: true | false;
+  tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("we are comeIn");
+    } else {
+      console.log("Your key is not valid");
+    }
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +46,13 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
+//Варіант коли ключ не пасує!!!!
+// const key = new Key();
+// const house = new MyHouse(new Key());
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
 
 export {};
